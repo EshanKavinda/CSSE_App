a7c64140557707ea03e4ada85203b4d26c07f114
EshanKavinda <eshan98kavinda@gmail.com>
2020-10-17 12:39:52 +0530

    add req ui

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import Dashboard from './component/dashboard';
 import Viewer from './component/Pages/viewR';
 import {} from 'react-native';
 import LogIn from './component/login';
+import AddItem from './component/Pages/addItem';
+import Apitest from './component/apiTest';
 
 const App: () => React$Node = () => {
   return <LogIn />;
 };
-
 export default App;
